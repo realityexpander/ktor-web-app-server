f3d87855360cb5bc6c92208dad3141eb18bb6a6a
Chris Athanas <realityexpander@gmail.com>
2023-07-14 01:33:22 -0500

    Convert Result

--- src/main/kotlin/common/Result.kt
+++ src/main/kotlin/common/Result.kt
@@ -1,59 +1,50 @@
-package common;
+package common
 
 /**
  * Result Utility class.
- * <p>
+ *
+ *
  * Utility class to hold a result of an operation.
  *
- * @param <T> Type of the result.<br>
- * Example:<br>
- * <pre>
- * Result&lt;String&gt; result = new Result.Success&lt;&gt;("Hello");
- * </pre>
+ * ```
  * Example:
- * <pre>
- * Result&lt;String&gt; result = new Result.Failure&lt;&gt;(new Exception("Error"));
- * </pre>
  *
+ * val result: Result<String> = Result.Success("Hello")
+ *
+ * Example:
+ *
+ * val result: Result<String> = Result.Failure(new Exception("Error"))
+ * ```
+ *
+ * @param <T> Type of the result.<br></br>
  * @author Chris Athanas (realityexpanderdev@gmail.com)
  * @since 0.11
  */
 
-@SuppressWarnings("unused")  // For T type parameter, which is actually used in Success<T> and Failure<T> subclasses...(todo - Why do we get that warning here?)
-public class Result<T> {
-    public static class Success<T> extends Result<T> {
-        private final T value;
+open class Result<T> {
+    class Success<T>(value: T) : Result<T>() {
+        private val value: T?
 
-        public Success(T value) {
-            this.value = value;
+        init {
+            this.value = value
         }
 
-        public T value() {
-            return value;
+        fun value(): T? {
+            return value
         }
 
-        public String toString() {
-            if (value == null)
-                return "null";
-            return value.toString();
+        override fun toString(): String {
+            return value?.toString() ?: "null"
         }
     }
 
-    public static class Failure<T> extends Result<T> {
-        private final Exception exception;
-
-        public Failure(Exception exception) {
-            this.exception = exception;
+    class Failure<T>(private val exception: Exception?) : Result<T>() {
+        fun exception(): Exception? {
+            return exception
         }
 
-        public Exception exception() {
-            return exception;
-        }
-
-        public String toString() {
-            if (exception == null)
-                return "null";
-            return exception.getLocalizedMessage();
+        override fun toString(): String {
+            return if (exception == null) "null" else exception.localizedMessage
         }
     }
 }
